d62e6399c84ab6020d560a1c2a0546be38aec021
Ahmed Gaber <21stlimitless@gmail.com>
2019-06-02 05:30:57 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+def dictionary
+  dictionary = {
+  "hello" => "hi",
+  "to" => "2",
+  "two" => "2",
+  "too" => "2",
+  "for" => "4",
+  "four" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&",
+}
+end
+
+def word_substituter(tweet)
+  tweet_array = tweet.split(" ")
+  dictionary.each do |key, replacement|
+    tweet_array.each do |word|
+      if word == key
+        word.replace replacement
+      end
+    end
+  end
+  tweet_array.join(" ")
+end
+
+def bulk_tweet_shortener(tweet_array)
+  tweet_array.each do |tweet_from_array|
+    puts word_substituter(tweet_from_array)
+  end
+end
